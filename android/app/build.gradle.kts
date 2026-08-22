658e4a981c5ce7aabad2f92d47ce673d154844a1
Abivarman <abivarman1729@gmail.com>
2026-08-22 14:12:43 +0530

    initial flutter app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,10 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.optimistic_browser"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -27,8 +24,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -46,5 +41,5 @@ flutter {
 
 dependencies {
     implementation("androidx.webkit:webkit:1.15.0")
-    implementation("org.mozilla.geckoview:geckoview:155.0.20260728095222")
+    implementation("org.mozilla.geckoview:geckoview-nightly:155.0.20260728095222")
 }
